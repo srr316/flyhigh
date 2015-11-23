a743a2afda7cc462f4630ffef844b4814e32fc5e
Johannes Link <jdl088@mun.ca>
2015-11-23 18:47:48 -0330

    requestHandlers.js

--- requestHandlers.js
+++ requestHandlers.js
@@ -8,7 +8,7 @@ var querystring = require("querystring");
 function start(response, postData){
 	console.log("Request handler 'start' was called");
 	
-	fs.readFile("Assignment3.html", "binary", function(error, file){
+	fs.readFile("index.html", "binary", function(error, file){
 		if(error){
 			response.writeHead(500, {"Content-Type": "text/plain"});
 			response.write(err + "\n");
